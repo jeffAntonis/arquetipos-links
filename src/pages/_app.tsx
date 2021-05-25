769d9441620acdbd47ede1f522c4c6a5e5a55603
jeffAntonis <antunis356@gmail.com>
2021-05-25 14:46:48 -0300

    fix: env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
-import { loadEnvConfig } from '@next/env'
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const projectDir = process.cwd()
-  loadEnvConfig(projectDir)
   
   return (
     <div className={styles.wrapper}>
